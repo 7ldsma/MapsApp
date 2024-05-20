3136aef5b72483f89d29497fd2758f7120e16f66
Alvaro <alvaro.ledesmav@gmail.com>
2024-05-20 17:43:53 +0200

    mapbox installed

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import mapboxgl from 'mapbox-gl';
+
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoiN2xkc21hIiwiYSI6ImNscjBuMG5uczBlMWkybXF2cXBmbnJzaWYifQ.htNmT4gF15bDVyiV289kAg';
+
+
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
