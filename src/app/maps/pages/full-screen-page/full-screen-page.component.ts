d7a69c3502f1403bfd7110880784af11c8a3c859
Alvaro <alvaro.ledesmav@gmail.com>
2024-05-21 02:14:07 +0200

    zoom range completed. markets added

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -18,11 +18,13 @@ export class FullScreenPageComponent implements AfterViewInit{
     const map = new Map({
 	  container: this.divMap.nativeElement, // container ID
 	  style: 'mapbox://styles/mapbox/streets-v12', // style URL
-	  center: [-74.5, 40], // starting position [lng, lat]
+	  center: [2.164, 41.389], // starting position [lng, lat]
 	  zoom: 9, // starting zoom
     });
   }
 
+
+  
   
 
 }
--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,128 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
+
+
+interface MarkerAndColor {
+  color: string,
+  marker: Marker,
+}
+
+
 
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrl: './markers-page.component.css'
 })
 export class MarkersPageComponent {
+
+  @ViewChild('map') divMap?: ElementRef;
+
+  public markers: MarkerAndColor[] = [];
+
+  public zoom: number = 12;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(2.164, 41.389);
+
+
+
+  ngAfterViewInit(): void {
+
+    if ( !this.divMap ) throw 'El elemento HTML no fue encontrado';
+
+    this.map = new Map({
+	  container: this.divMap.nativeElement, // container ID
+	  style: 'mapbox://styles/mapbox/streets-v12', // style URL
+	  center: this.currentLngLat, // starting position [lng, lat]
+	  zoom: this.zoom, // starting zoom
+    });
+
+
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Alvaro Ledesma'
+
+
+    // const marker = new Marker({
+
+    //   element: markerHtml,
+    //   color: 'green'
+    // })
+    //   .setLngLat( this.currentLngLat )
+    //   .addTo( this.map );
+
+    
+  }
+  
+
+  createMarker() {
+
+    if ( !this.map ) return;
+    const color = '#xxxxxx'.replace(/x/g, y => (Math.random()*16|0).toString(16));
+
+    const lngLat = this.map.getCenter();
+
+    this.addMarker( lngLat, color);
+  }
+
+
+  
+  addMarker( lngLat: LngLat, color: string) {
+
+    if ( !this.map ) return;
+
+    const marker = new Marker({
+      color: color,
+      draggable: true
+    })
+      .setLngLat( lngLat)
+      .addTo( this.map )
+
+      this.markers.push({
+        color,
+        marker,
+      });  //agrego cada marker que creo en el array de markers
+  }
+
+
+  deleteMarker( index: number ){
+    this.markers[ index ].marker.remove();
+    this.markers.splice( index, 1 );
+  }
+
+
+  flyTo( marker: Marker) {
+    this.map?.flyTo({
+      zoom: 14,
+      center: marker.getLngLat(),
+    })
+  }
+
+
+// para hacer persistentes los marcadores los siguientes metodos
+
+  saveToLocalStorage() {
+
+  }
+
+
+  readFromLocalStorage() {
+    
+  }
+
+
+
+
+  zoomIn(){
+    this.map?.zoomIn;
+  }
+
+  zoomOut(){
+    this.map?.zoomOut;
+  }
+
+  zoomChanged( value: string ){
+
+    this.zoom = Number(value);
+    this.map?.zoomTo( this.zoom ); 
+  }
 
 }
